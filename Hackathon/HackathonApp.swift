13372b3c067cad220fda9aa6f624a32027fd467f
Anastasia Harmon <ahh2018@icloud.com>
2024-12-19 09:31:24 -0500

    Initial Commit

---
+++ Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by Anastasia Harmon on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
