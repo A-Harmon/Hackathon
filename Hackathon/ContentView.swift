f886592c7e400ba9664ae74444802d4462010a3c
Anastasia Harmon <ahh2018@icloud.com>
2024-12-19 10:17:45 -0500

    Home created

--- Hackathon/ContentView.swift
+++ Hackathon/ContentView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image("horse")
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .frame(width: 100, height: 100)
+                    .padding()
+                Text("InstaGreek")
+                    .font(.largeTitle)
+                    .bold()
+                
+            }
+            Text("Home")
+                .font(.title2)
+            
+            ScrollView{
+                
+            }
         }
-        .padding()
+        Spacer()
     }
 }
 
