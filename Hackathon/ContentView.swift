ce935870ed402dd03a5a3c376f7ca96d440962c2
goromajima <goromajimaisreal12834@gmail.com>
2024-12-19 12:23:01 -0500

    tweets save

--- Hackathon/ContentView.swift
+++ Hackathon/ContentView.swift
@@ -8,8 +8,7 @@ import SwiftUI
 import UIKit
 
 struct ContentView: View {
-    @State var tweets: [User] = []
-    @State var loadTweets = false
+    @Binding var tweets: [User]
     var body: some View {
         NavigationStack {
             VStack {
@@ -29,38 +28,31 @@ struct ContentView: View {
                     .font(.title2)
                 
                 ScrollView{
-                    if loadTweets {
-                        ForEach(tweets.shuffled()) {usr in
-                            NavigationLink(value: usr, label: {
-                                IndivualTweetView(user: usr, tweet: usr.tweets[Int.random(in: 0...usr.tweets.count-1)])
-                            })
-                            .foregroundStyle(.black)
-                            Divider()
-                        }
-                        .navigationDestination(for: User.self, destination: {user in
-                            ProfileView(user: users[users.firstIndex(where: {$0.username == user.username})!])
+                    ForEach(tweets.shuffled()) {usr in
+                        NavigationLink(value: usr, label: {
+                            IndivualTweetView(user: usr, tweet: usr.tweets[Int.random(in: 0...usr.tweets.count-1)])
                         })
+                        .foregroundStyle(.black)
+                        Divider()
                     }
+                    .navigationDestination(for: User.self, destination: {user in
+                        ProfileView(user: users[users.firstIndex(where: {$0.username == user.username})!])
+                    })
                 }
                 .refreshable {
                     tweets.removeAll()
                     GetTweets()
                 }
             }
-            .onAppear {
-                GetTweets()
-            }
         }
     }
     func GetTweets() {
-        loadTweets = false
         for x in 10...Int.random(in: 20...40) {
             let usr = users[Int.random(in: 0...users.count-1)]
             tweets.append(User(displayName: usr.displayName, username: usr.username, profilePicture: usr.profilePicture, followers: usr.followers, tweets: usr.tweets))
         }
-        loadTweets = true
     }
 }
 #Preview {
-    ContentView()
+    ContentView(tweets: .constant([]))
 }
--- Hackathon/HackathonApp.swift
+++ Hackathon/HackathonApp.swift
@@ -9,28 +9,40 @@ import SwiftUI
 @main
 struct HackathonApp: App {
     @State var view = "home"
+    @State var tweets: [User] = []
     var body: some Scene {
         WindowGroup {
-            if view == "home" {
-                ContentView()
-            }
-            if view == "newTweet" {
-                WriteView()
-            }
-            if view == "profile" {
-                ProfileView(user: odysseus)
-            }
-            Spacer()
-            HStack {
+            VStack {
+                if view == "home" {
+                    ContentView(tweets: $tweets)
+                }
+                if view == "newTweet" {
+                    WriteView()
+                }
+                if view == "profile" {
+                    ProfileView(user: odysseus)
+                }
                 Spacer()
-                Button(action: {view="home"}, label: {Image(systemName: "house.fill").foregroundStyle(Color.main)})
-                Spacer()
-                Button(action: {view="newTweet"}, label: {Image(systemName: "plus.circle.fill").font(.title).foregroundStyle(Color.main)})
-                Spacer()
-                Button(action: {view="profile"}, label: {Image(systemName: "person.fill").foregroundStyle(Color.main)})
-                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {view="home"}, label: {Image(systemName: "house.fill").foregroundStyle(Color.main)})
+                    Spacer()
+                    Button(action: {view="newTweet"}, label: {Image(systemName: "plus.circle.fill").font(.title).foregroundStyle(Color.main)})
+                    Spacer()
+                    Button(action: {view="profile"}, label: {Image(systemName: "person.fill").foregroundStyle(Color.main)})
+                    Spacer()
+                }
+                .frame(maxWidth:.infinity,maxHeight: 50)
             }
-            .frame(maxWidth:.infinity,maxHeight: 50)
+            .onAppear {
+                GetTweets()
+            }
+        }
+    }
+    func GetTweets() {
+        for x in 10...Int.random(in: 20...40) {
+            let usr = users[Int.random(in: 0...users.count-1)]
+            tweets.append(User(displayName: usr.displayName, username: usr.username, profilePicture: usr.profilePicture, followers: usr.followers, tweets: usr.tweets))
         }
     }
 }
